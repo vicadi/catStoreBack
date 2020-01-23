b9c6a3e43467f240d0fc18e98173e3cfadd4abd3
vicadi <vicadi7@gmail.com>
2020-01-23 17:18:07 -0500

    create proyect

---
+++ src/main/java/com/catstore/servicestore/ServicestoreApplication.java
@@ -0,0 +1,13 @@
+package com.catstore.servicestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicestoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicestoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/catstore/servicestore/ServicestoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catstore.servicestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicestoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
